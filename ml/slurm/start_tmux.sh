09d3b152f8307f0780f9d4cfa7c94f458e0ee956
shehryarkh4n <shehryar.khan2580@gmail.com>
2026-04-06 20:00:49 -0400

    fix ChromaDB telemetry bug using flag

--- ml/slurm/start_tmux.sh
+++ ml/slurm/start_tmux.sh
@@ -71,6 +71,7 @@ echo 'Waiting 60s for vLLM to load model...' && \
 sleep 60 && \
 export HPEMA_CONFIG=$CONFIG && \
 export HPEMA_API_KEY=unused && \
+export ANONYMIZED_TELEMETRY=False && \
 python -m cli.main" Enter
 
 # Attach
